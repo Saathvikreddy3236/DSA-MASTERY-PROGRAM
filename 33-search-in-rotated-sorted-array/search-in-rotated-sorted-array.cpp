f56e9c778d0650c8c6345b0b9218893b658e0f04
Saathvikreddy Dandu <dandusaathvikreddy2006@gmail.com>
2026-08-24 09:46:25 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (96.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,12 +1,36 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int t) {
-        unordered_map<int, int> mp;
-        for (int i = 0; i < nums.size(); i++) {
-            mp[nums[i]] = i;
+    int search(vector<int>& nums, int target) {
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target)
+                return mid;
+            if(nums[left] <= nums[mid]) {
+
+                if(nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+
+            else {
+                if(nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
         }
-        if (mp.find(t) != mp.end())
-            return mp[t];
+
         return -1;
     }
 };
