813174d4528aa756d038107411d6f45e6b0d5fc4
Saathvikreddy Dandu <dandusaathvikreddy2006@gmail.com>
2026-09-07 23:34:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (94.21%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,25 +1,67 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        map<string, int> m{{"I", 1},   {"IV", 4},   {"V", 5},   {"IX", 9},
-                           {"X", 10},  {"XL", 40},  {"L", 50},  {"XC", 90},
-                           {"C", 100}, {"CD", 400}, {"D", 500}, {"CM", 900},
-                           {"M", 1000}};
-        int sum = 0, f = 0;
+        int sum = 0;
+
         for (int i = 0; i < s.length(); i++) {
-            string c = "";
-            c += s[i];
-            if (i + 1 < s.length()) {
-                c += s[i + 1];
-                if (m.contains(c)) {
-                    sum += m[c];
-                    i++;
-                    continue;
-                }
-                c.pop_back();
+            int curr, next = 0;
+
+            switch (s[i]) {
+            case 'I':
+                curr = 1;
+                break;
+            case 'V':
+                curr = 5;
+                break;
+            case 'X':
+                curr = 10;
+                break;
+            case 'L':
+                curr = 50;
+                break;
+            case 'C':
+                curr = 100;
+                break;
+            case 'D':
+                curr = 500;
+                break;
+            case 'M':
+                curr = 1000;
+                break;
             }
-            sum += m[c];
+
+            if (i + 1 < s.length()) {
+                switch (s[i + 1]) {
+                case 'I':
+                    next = 1;
+                    break;
+                case 'V':
+                    next = 5;
+                    break;
+                case 'X':
+                    next = 10;
+                    break;
+                case 'L':
+                    next = 50;
+                    break;
+                case 'C':
+                    next = 100;
+                    break;
+                case 'D':
+                    next = 500;
+                    break;
+                case 'M':
+                    next = 1000;
+                    break;
+                }
+            }
+
+            if (curr < next)
+                sum -= curr;
+            else
+                sum += curr;
         }
+
         return sum;
     }
 };
